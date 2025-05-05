19bc327cf0044eecc2201fad36e8e5cdb6c00bf4
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-05-05 09:33:51 +0100

    Update Section5.tsx

--- components/Section5.tsx
+++ components/Section5.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Section5 = () => {
   return (
-    <div className='md:mx-40 mx-3 flex flex-col md:flex-row items-center justify-between my-10 md:my-32 ' >
+    <div className='md:mx-40 mx-3 flex flex-col md:flex-row items-center justify-between my-10 md:my-32 ' id="integration" >
           <div className="left-side md:w-[40%] my-10 ">
             <div className="title  text-2xl md:text-6xl font-Poppins-bold xl:leading-[80px] ">Seamless <span className="text-primary" >API Integration</span></div>
             <div className="info text-xl w-[80%] ">Connect MedScan with your existing ERP or inventory systems via our flexible API. Quick to deploy and easy to scale.</div>
