2aca2272b36130d6c66adaa4538c31b39bf0a39d
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-05-05 09:31:15 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,17 +2,18 @@
 import Image from 'next/image'
 import React, {useState, } from 'react'
 import Button from './Button'
+import {Link as ScrollLink} from 'react-scroll'
 import { BiArrowBack, BiMenuAltRight, BiX, } from 'react-icons/bi'
 
 const Header = () => {
   const [open, setOpen] = useState<boolean>(false)
 
   const nav = [
-    {name:'Home', route:'#'},
-    {name:'Features', route:'#'},
-    {name:'How It Works', route:'#'},
-    {name:'Integration', route:'#'},
-    {name:'Support', route:'#'},
+    {name:'Home', route:'hero'},
+    {name:'Features', route:'features'},
+    {name:'How It Works', route:'works'},
+    {name:'Integration', route:'integration'},
+    {name:'Support', route:'banner'},
   ]
   return (
     <div className='bg-white mt-5 md:mt-20 w-[90%] sm:w-[80%] relative rounded-full mx-auto px-5 py-3 flex flex-row justify-between items-center ' >
@@ -25,11 +26,17 @@ const Header = () => {
   
     </div>
 
-      <nav className={`flex max-xl:absolute max-md:right-0 max-xl:right-20 z-[9999] max-xl:top-20 ${open? 'flex' : 'max-xl:hidden' } max-xl:rounded-xl max-xl:max-w-[600px] max-xl:w-full max-xl:bg-primary max-xl:px-5   max-xl:text-white flex-1 flex-col xl:flex-row justify-evenly `} >
+      <nav className={`flex max-xl:absolute max-sm:scale-75 max-md:right-0 max-xl:right-20 z-[9999] max-xl:top-20 ${open? 'flex' : 'max-xl:hidden' } max-xl:rounded-xl max-xl:max-w-[600px] max-xl:w-full max-xl:bg-primary max-xl:px-5   max-xl:text-white flex-1 flex-col xl:flex-row justify-evenly `} >
         <ul className='flex flex-col xl:flex-row items-center ' >
           {nav.map((data, index) => (
+      <ScrollLink to={nav.route}  activeClass="active"
+              smooth={true}
+              spy={true}
+              key={index}
+              offset={-150}>
               <li  key={data.name} className='mx-5 max-xl:border max-xl:border-white max-xl:rounded-full max-xl:w-full max-xl:py-4 max-xl:text-center max-xl:m-3  text-xl font-bold cursor-pointer ' >{data.name}</li>
-          ))}
+           </ScrollLink>
+              ))}
         </ul>
       <div><Button text='Book A Demo' icon={<BiArrowBack className='rotate-180 ' /> } className='max-xl:bg-white mx-auto max-xl:my-3 max-xl:text-primary' /></div>
       </nav>
