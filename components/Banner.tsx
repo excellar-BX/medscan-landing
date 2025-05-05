4d248d6496544177c22ff4b7f346d06a834d8a39
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-05-05 09:34:31 +0100

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import Button from './Button'
 
 const Banner = () => {
   return (
-    <div className="bg-primary bg-opacity-20 py-5 md:py-32 my-5 max-md:px-2  " >
+    <div className="bg-primary bg-opacity-20 py-5 md:py-32 my-5 max-md:px-2  " id="banner" >
        <div className="md:text-5xl text-3xl font-Poppins-bold md:w-[50%] md:text-center mx-auto my-5 xl:leading-[60px] " >Ready to simplify compliance and <span className="text-primary" >protect your brand?</span> </div>
        <div className='md:text-center md:w-[50%] mx-auto text-xl' >Join the growing network of manufacturers and distributors using MedScan to fight counterfeiting and build trust. </div>
        
