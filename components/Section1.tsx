9fc69e02bbc25647ca8d3ec204c550c70197b0d1
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-05-05 09:32:01 +0100

    Update Section1.tsx

--- components/Section1.tsx
+++ components/Section1.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Section1 = () => {
   return (
-    <div className='md:mx-40 mx-3 flex flex-col md:flex-row items-center justify-between my-10 md:my-32 ' >
+    <div className='md:mx-40 mx-3 flex flex-col md:flex-row items-center justify-between my-10 md:my-32 ' id="features">
       <div className="left-side md:w-[40%] my-10 ">
         <div className="title text-2xl md:text-6xl font-Poppins-bold xl:leading-[80px] ">Full <span className="text-primary" >Supply Chain Tracking</span></div>
         <div className="info text-xl sm:w-[80%] ">Track each product's full journey from production to delivery in real time. Gain visibility at every stage to ensure accountability and reduced risk.</div>
